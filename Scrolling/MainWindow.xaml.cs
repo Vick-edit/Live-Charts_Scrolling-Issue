23285bee7605bcbf86e59c5956f0276eaf586112
Viktor.Mironov <vicktor.mironov@gmail.com>
2017-11-30 17:13:17 +0500

    Initial commit

---
+++ Scrolling/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts.Events;
+
+namespace Scrolling
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowModel();
+        }
+
+        private void Axis_OnRangeChanged(RangeChangedEventArgs eventargs)
+        {
+            var viewModel = (MainWindowModel)DataContext;
+
+            var currentRange = eventargs.Range;
+
+            if (currentRange < TimeSpan.TicksPerDay * 2)
+            {
+                viewModel.Formatter = x => new DateTime((long)x).ToString("t");
+                return;
+            }
+
+            if (currentRange < TimeSpan.TicksPerDay * 60)
+            {
+                viewModel.Formatter = x => new DateTime((long)x).ToString("dd MMM yy");
+                return;
+            }
+
+            if (currentRange < TimeSpan.TicksPerDay * 540)
+            {
+                viewModel.Formatter = x => new DateTime((long)x).ToString("MMM yy");
+                return;
+            }
+
+            viewModel.Formatter = x => new DateTime((long)x).ToString("yyyy");
+        }
+
+        private void Axis_OnPreviewRangeChanged(PreviewRangeChangedEventArgs e)
+        {
+            var viewModel = (MainWindowModel)DataContext;
+
+            var percent = viewModel.MaxRange * 0.3;
+            if (e.PreviewMinValue < viewModel.MinValueX - percent)
+                e.Cancel = true;
+            if (e.PreviewMaxValue > viewModel.MaxValueX + percent)
+                e.Cancel = true;
+        }
+    }
+}
---
+++ Scrolling/MainWindowModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using LiveCharts;
+using LiveCharts.Defaults;
+
+namespace Scrolling
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        public object Mapper { get; set; }
+        public ChartValues<DateTimePoint> Values { get; set; }
+
+        public double MinValueX => Values.Min(x => x.DateTime).Ticks;
+        public double MaxValueX => Values.Max(x => x.DateTime).Ticks;
+        public double MaxRange => (MaxValueX - MinValueX) * 1.1;
+
+
+        public MainWindowModel()
+        {
+            Values = new ChartValues<DateTimePoint>();
+
+            var trend = 50d;
+            var random = new Random();
+            var timeStep = DateTime.Now;
+            for (var i = 0; i < 500; i++)
+            {
+                timeStep = timeStep.AddHours(1);
+                Values.Add(new DateTimePoint(timeStep.AddDays(i*10), trend));
+
+                if (random.NextDouble() > 0.4)
+                {
+                    trend += random.NextDouble()*10;
+                }
+                else
+                {
+                    trend -= random.NextDouble()*10;
+                }
+            }
+
+            Formatter = x => new DateTime((long) x).ToString("yyyy");
+            From = DateTime.Now.AddHours(10000).Ticks;
+            To = DateTime.Now.AddHours(90000).Ticks;
+        }
+
+        #region ViewModelProperties
+        private Func<double, string> _formatter;
+        public Func<double, string> Formatter
+        {
+            get { return _formatter; }
+            set
+            {
+                _formatter = value;
+                OnPropertyChanged(nameof(Formatter));
+            }
+        }
+
+        private double _from;
+        public double From
+        {
+            get { return _from; }
+            set
+            {
+                _from = value;
+                OnPropertyChanged(nameof(From));
+            }
+        }
+
+        private double _to;
+        public double To
+        {
+            get { return _to; }
+            set
+            {
+                _to = value;
+                OnPropertyChanged(nameof(To));
+            }
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
